a4c28b835ba0aa7611e70f3fbbef2e89e503bd03
mirashif <mir.ashif@outlook.com>
2021-08-05 15:19:14 +0600

    Added Offer

--- src/Home/Restaurant/Menu.tsx
+++ src/Home/Restaurant/Menu.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import * as Linking from "expo-linking";
-import { ImageBackground, TouchableWithoutFeedback } from "react-native";
+import { ImageBackground } from "react-native";
 
-import { Box, CircularIcon, SafeArea, Text, useTheme } from "../../components";
+import { SafeArea, useTheme } from "../../components";
 
 import Header from "./Header";
+import Offer from "./Offer";
 
 const Menu = () => {
   const theme = useTheme();
@@ -22,51 +22,12 @@ const Menu = () => {
       >
         <Header title="Cheez" />
       </ImageBackground>
-      <Box
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          paddingHorizontal: 30,
-          top: -28,
-        }}
-      >
-        <Box
-          style={{
-            flex: 1,
-            borderRadius: theme.borderRadii.l,
-            backgroundColor: theme.colors.primary,
-            paddingVertical: 12,
-            paddingHorizontal: 18,
-            marginRight: 8,
-          }}
-        >
-          <Text
-            style={{
-              fontFamily: "Bold",
-              fontSize: 16,
-              color: theme.colors.primaryContrast,
-            }}
-          >
-            20% OFF
-          </Text>
-          <Text
-            style={{
-              fontFamily: "Normal",
-              fontSize: 12,
-              color: theme.colors.primaryContrast,
-            }}
-          >
-            Enjoy 20% OFF on the entire menu!
-          </Text>
-        </Box>
-        <TouchableWithoutFeedback
-          onPress={() => {
-            Linking.openURL("tel:8777111223");
-          }}
-        >
-          <CircularIcon name="phone" size={58} />
-        </TouchableWithoutFeedback>
-      </Box>
+
+      <Offer
+        title="20% OFF"
+        description="Enjoy 20% OFF on the entire menu!"
+        telephone="8777111223"
+      />
     </SafeArea>
   );
 };
