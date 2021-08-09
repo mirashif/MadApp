0b7132d3fb9216827a29706290e6d783322f60d3
samiulhsohan <samiulhsohan@gmail.com>
2021-08-09 08:12:28 +0600

    add Mobile Number screen

---
+++ src/Auth/MobileNumber.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { TextInput } from "react-native";
+
+import { Box, HeaderBar, SafeArea, Text } from "../components";
+import DissmissKeyboard from "../components/DissmissKeyboard";
+
+import Button from "./Button";
+import Phone from "./assets/Phone.svg";
+import BDFlag from "./assets/BDFlag.svg";
+
+const MobileNumber = () => {
+  return (
+    <SafeArea>
+      <HeaderBar title="Your Details" onBackPress={() => null} />
+
+      <DissmissKeyboard>
+        <Box flex={1} px="screen">
+          <Box flex={1} justifyContent="space-between">
+            <Box>
+              <Box alignItems="center" style={{ marginTop: 32 }}>
+                <Phone />
+              </Box>
+
+              <Text
+                fontSize={24}
+                fontFamily="Bold"
+                textAlign="center"
+                style={{ color: "#323232", marginTop: 26 }}
+              >
+                What’s Your Mobile Number?
+              </Text>
+
+              <Box alignItems="center" my="l">
+                <Box width={288}>
+                  <Text style={{ color: "#BBBBBB" }} textAlign="center">
+                    We need this to verify and secure your account
+                  </Text>
+                </Box>
+              </Box>
+
+              <Box
+                flexDirection="row"
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Box flexDirection="row" alignItems="center">
+                  <BDFlag />
+                  <Text
+                    fontSize={18}
+                    style={{ color: "#111111", marginLeft: 8 }}
+                  >
+                    +88
+                  </Text>
+                </Box>
+
+                <Box ml="m">
+                  <TextInput
+                    keyboardType="number-pad"
+                    style={{
+                      borderColor: "#DDDDDD",
+                      borderWidth: 1,
+                      width: 220,
+                      padding: 14,
+                      fontSize: 18,
+                      borderRadius: 12,
+                    }}
+                  />
+                </Box>
+              </Box>
+            </Box>
+
+            <Box style={{ paddingTop: 16, paddingBottom: 40 }}>
+              <Button title="Continue" onPress={() => null} />
+            </Box>
+          </Box>
+        </Box>
+      </DissmissKeyboard>
+    </SafeArea>
+  );
+};
+
+export default MobileNumber;
