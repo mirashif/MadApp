4afe71492ff3a43a0886f20e71bed7862b30862a
samiulhsohan <samiulhsohan@gmail.com>
2021-08-13 17:46:47 +0600

    add profile

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,24 +1,39 @@
 import React from "react";
-import { TextInput } from "react-native";
+import { StyleProp, TextStyle, TextInput } from "react-native";
+
+import { Box, Text } from "./theme";
 
 interface InputProps {
+  style?: StyleProp<TextStyle>;
+  label?: string;
   placeholder?: string;
   onChangeText: (vale: string) => void;
 }
 
-const Input = ({ placeholder, onChangeText }: InputProps) => {
+const Input = ({ placeholder, onChangeText, label, style }: InputProps) => {
   return (
-    <TextInput
-      onChangeText={(value) => onChangeText(value)}
-      placeholder={placeholder}
-      style={{
-        borderColor: "#DDDDDD",
-        borderWidth: 1,
-        padding: 16,
-        fontSize: 18,
-        borderRadius: 12,
-      }}
-    />
+    <Box>
+      {label && (
+        <Text mb="m" fontFamily="Medium">
+          {label}
+        </Text>
+      )}
+
+      <TextInput
+        onChangeText={(value) => onChangeText(value)}
+        placeholder={placeholder}
+        style={[
+          {
+            borderColor: "#DDDDDD",
+            borderWidth: 1,
+            padding: 16,
+            fontSize: 18,
+            borderRadius: 12,
+          },
+          style,
+        ]}
+      />
+    </Box>
   );
 };
 
