ee0dd4073435cd6ef988d09127394cf895e93661
mirashif <mir.ashif@outlook.com>
2021-08-18 20:09:11 +0600

    Added menu content scroll y

--- src/Home/Restaurant/Menu.tsx
+++ src/Home/Restaurant/Menu.tsx
@@ -1,22 +1,57 @@
 import React, { useRef, useState } from "react";
-import { ScrollView, TouchableWithoutFeedback } from "react-native";
+import { Image, ScrollView, TouchableWithoutFeedback } from "react-native";
 
 import { Box, SafeArea, Text } from "../../components";
 
-const menu = [
-  { name: "Classic Pizzas" },
-  { name: "Gourmet Pizzas" },
-  { name: "Tomato Soups" },
-  { name: "Potato Mehedi" },
-  // { name: "Classic Pizzas2" },
-  // { name: "Gourmet Pizzas2" },
-  // { name: "Tomato Soups2" },
-  // { name: "Potato Mehedi2" },
+const items = [
+  {
+    name: "Long Hongdae Nights",
+    description:
+      "Korean fried chicken glazed with Gochujang, garnished with sesame & spring onions, served with fries & Miss Miu Mayo",
+    price: "৳ 26",
+    image: "https://source.unsplash.com/a66sGfOnnqQ",
+  },
+  {
+    name: "Late Sunset",
+    description:
+      "Korean fried chicken starter with dirty cheese sauce and Artisan Hot Sauce - the naughty version new, favourite",
+    price: "৳ 13.50",
+    image: "https://source.unsplash.com/a66sGfOnnqQ",
+  },
+  {
+    name: "Cabbage Kimchi",
+    description: "Portion, vegan",
+    price: "৳ 5.00",
+    image: "https://source.unsplash.com/a66sGfOnnqQ",
+  },
+  {
+    name: "Namur by Pieces",
+    description:
+      "Homemade steamed dim sum with minced pork, shiitake mushrooms and smokey honey flavour, four pcs",
+    price: "৳ 10.50",
+    image: "https://source.unsplash.com/a66sGfOnnqQ",
+  },
+  {
+    name: "Silim Lights",
+    description:
+      "Beef Bibimbap, sesame oil, rice, beans, spinach, carrots, spring onions, Chinese cabbage, shiitake mushrooms, roasted onions and egg",
+    price: "৳ 26.50",
+    image: "https://source.unsplash.com/a66sGfOnnqQ",
+  },
 ];
 
-const tabs = menu.map(({ name }) => ({ name }));
+const menu = [
+  { name: "Classic Pizzas", items },
+  { name: "Gourmet Pizzas", items },
+  { name: "Tomato Soups", items },
+  { name: "Potato Mehedi", items },
+];
+
+export const defaultTabs = menu.map(({ name }) => ({ name, anchor: 0 }));
 
 const Menu = () => {
+  const [tabs, setTabs] = useState(defaultTabs);
+
   const [measurements, setMeasurements] = useState<number[]>(
     new Array(tabs.length).fill(0)
   );
@@ -25,59 +60,157 @@ const Menu = () => {
   );
 
   const TabScrollViewRef = useRef<ScrollView>(null);
+  const ContentScrollViewRef = useRef<ScrollView>(null);
 
-  const handleTabScroll = (index: number) => {
+  const handleAutoScroll = (index: number) => {
     TabScrollViewRef.current?.scrollTo({
       x: anchors[index],
       y: 0,
       animated: true,
     });
+
+    ContentScrollViewRef.current?.scrollTo({
+      x: 0,
+      y: tabs[index].anchor,
+      animated: true,
+    });
+  };
+
+  const onMeasurement = (
+    index: number,
+    tab: { name: string; anchor: number }
+  ) => {
+    tabs[index] = tab;
+    setTabs([...tabs]);
   };
 
   return (
     <SafeArea>
-      <Box flexDirection="row" my="xl">
-        <ScrollView
-          ref={TabScrollViewRef}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-        >
-          {tabs.map((tab, tabIndex) => (
-            <TouchableWithoutFeedback
-              onPress={() => handleTabScroll(tabIndex)}
-              key={tabIndex}
+      {/* TabHeader */}
+      <ScrollView
+        contentContainerStyle={{
+          flexDirection: "row",
+        }}
+        ref={TabScrollViewRef}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      >
+        {tabs.map((tab, tabIndex) => (
+          <TouchableWithoutFeedback
+            onPress={() => handleAutoScroll(tabIndex)}
+            key={tabIndex}
+          >
+            <Box
+              onLayout={({
+                nativeEvent: {
+                  layout: { width },
+                },
+              }) => {
+                const _measurements = measurements;
+                const _anchors = anchors;
+
+                _measurements[tabIndex] = Math.round(width);
+                setMeasurements([..._measurements]);
+
+                let tabAnchor = 0;
+                _anchors.forEach((_, anchorIndex) => {
+                  if (anchorIndex < tabIndex) {
+                    tabAnchor += _measurements[tabIndex];
+                  }
+                });
+                _anchors[tabIndex] = tabAnchor;
+                setAnchors([..._anchors]);
+              }}
+              py="m"
+              px="xl"
+              backgroundColor="background"
             >
-              <Box
-                onLayout={({
-                  nativeEvent: {
-                    layout: { width },
-                  },
-                }) => {
-                  const _measurements = measurements;
-                  const _anchors = anchors;
+              <Text color="dark">• {tab.name}</Text>
+            </Box>
+          </TouchableWithoutFeedback>
+        ))}
+      </ScrollView>
 
-                  _measurements[tabIndex] = Math.round(width);
-                  setMeasurements([..._measurements]);
-
-                  let tabAnchor = 0;
-                  _anchors.forEach((_, anchorIndex) => {
-                    if (anchorIndex < tabIndex) {
-                      tabAnchor += _measurements[tabIndex];
-                    }
-                  });
-                  _anchors[tabIndex] = tabAnchor;
-                  setAnchors([..._anchors]);
-                }}
-                py="m"
-                px="xl"
-                backgroundColor="primary"
-              >
-                <Text color="primaryContrast">{tab.name}</Text>
-              </Box>
-            </TouchableWithoutFeedback>
-          ))}
-        </ScrollView>
-      </Box>
+      {/* Content */}
+      <ScrollView
+        ref={ContentScrollViewRef}
+        showsVerticalScrollIndicator={false}
+      >
+        {menu.map(({ name: menuName, items: menuItems }, i) => (
+          <Box
+            style={{ paddingVertical: 15 }}
+            key={i}
+            onLayout={({
+              nativeEvent: {
+                layout: { y: anchor },
+              },
+            }) =>
+              onMeasurement(i, {
+                name: menuName,
+                anchor,
+              })
+            }
+          >
+            <Text
+              style={{
+                fontSize: 18,
+                fontFamily: "Normal",
+                color: "red",
+                marginBottom: 4,
+                marginHorizontal: 8,
+              }}
+            >
+              {menuName}
+            </Text>
+            <Box
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                flexWrap: "wrap",
+              }}
+            >
+              {menuItems.map(({ image, name, price }, j) => (
+                <Box
+                  style={{
+                    margin: 8,
+                  }}
+                  key={j}
+                >
+                  <Image
+                    style={{
+                      height: 150,
+                      width: 150,
+                      borderRadius: 12,
+                      marginBottom: 7,
+                    }}
+                    source={{ uri: image }}
+                  />
+                  <Text
+                    numberOfLines={1}
+                    style={{
+                      fontFamily: "Bold",
+                      fontSize: 11,
+                      color: "black",
+                    }}
+                  >
+                    {name}
+                  </Text>
+                  <Text
+                    numberOfLines={1}
+                    style={{
+                      fontFamily: "Normal",
+                      fontSize: 11,
+                      color: "gray",
+                    }}
+                  >
+                    {price}
+                  </Text>
+                </Box>
+              ))}
+            </Box>
+          </Box>
+        ))}
+      </ScrollView>
     </SafeArea>
   );
 };
