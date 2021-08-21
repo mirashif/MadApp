1357c724b7d076e8e827a27d7626d6b0419cd8ca
mirashif <mir.ashif@outlook.com>
2021-08-21 20:45:15 +0600

    Fix getNode() warning

--- App.tsx
+++ App.tsx
@@ -2,10 +2,15 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import { LogBox } from "react-native";
 
 import AppNavigator from "./src/components/AppNavigator";
 import { assets as CashbackAssets } from "./src/Cashback";
 import { LoadAssets, ThemeProvider } from "./src/components";
+
+LogBox.ignoreLogs([
+  "ReactNativeFiberHostComponent: Calling getNode() on the ref of an Animated component is no longer necessary. You can now directly use the ref instead. This method will be removed in a future release.",
+]);
 
 const assets = [...CashbackAssets];
 
