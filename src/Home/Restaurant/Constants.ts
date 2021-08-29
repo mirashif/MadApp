544365ab5fdd014fe9308f86bac6cbf222c4808f
mirashif <mir.ashif@outlook.com>
2021-08-29 14:45:06 +0600

    ❌

--- src/Home/Restaurant/Constants.ts
+++
@@ -1,9 +0,0 @@
-export const HEADER_IMAGE_HEIGHT = 220;
-export const HEADER_HEIGHT = 52;
-
-export interface TabModel {
-  name: string;
-  width: number;
-  anchor: number;
-  contentAnchor: number;
-}
