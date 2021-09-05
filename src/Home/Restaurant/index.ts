d8ae58635cd49a62391ec4c404dad05b480d6ed4
Mir Ashif <34467318+mirashif@users.noreply.github.com>
2021-09-05 16:36:13 +0600

    Tabheader (#9)
    * 🛠
    
    * 🛠
    
    * 🛠
    
    * 🛠
    
    * ✅ tab anchor
    
    * Added fn to calculate tab anchor
    
    * Added menu content scroll y
    
    * 🛠 handle on scroll
    
    * 🛠
    
    * Fix getNode() warning
    
    * 🛠
    
    * Added automatic scroll update
    
    * 🛠
    
    * 🥴
    
    * ✅
    
    * Renamed to contentAnchor
    
    * ✅
    
    * 🛠
    
    * 🛠
    
    * 🛠
    
    * 🤺
    
    * DEL Restaurant to revert changes
    
    * ❌
    
    * tsconfig for redash lib
    
    * copied over

--- src/Home/Restaurant/index.ts
+++ src/Home/Restaurant/index.ts
@@ -1,1 +1,8 @@
-export { default as Menu } from "./Menu";
+import { backgroundImage } from "./HeaderImage";
+
+export { default as Menu } from "./UberEats";
+export const assets = [backgroundImage];
+export const fonts = {
+  UberMoveRegular: require("./assets/UberMoveRegular.ttf"),
+  UberMoveMedium: require("./assets/UberMoveMedium.ttf"),
+};
