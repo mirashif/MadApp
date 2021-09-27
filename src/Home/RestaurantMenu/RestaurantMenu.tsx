f8a165a1e446ea1680ea620f04d7c73920757cb2
mirashif <mir.ashif@outlook.com>
2021-09-27 15:14:38 +0600

    gear

--- src/Home/RestaurantMenu/RestaurantMenu.tsx
+++ src/Home/RestaurantMenu/RestaurantMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { View } from "react-native";
 import type Animated from "react-native-reanimated";
 import { onScrollEvent, useValue } from "react-native-redash/lib/module/v1";
@@ -14,9 +14,8 @@ const RestaurantMenu = () => {
   const scrollViewRef = useRef<Animated.ScrollView>(null);
   const y = useValue(0);
   const onScroll = onScrollEvent({ y });
-
-  const anchorX: number[] = [];
-  const anchorY: number[] = [];
+  const [anchorX, setAnchorX] = useState<number[]>([]);
+  const [anchorY, setAnchorY] = useState<number[]>([]);
 
   return (
     <SafeArea>
@@ -26,19 +25,27 @@ const RestaurantMenu = () => {
       </View>
       <TabHeader
         onTabPress={(index: number) => {
+          console.log(index);
+          console.log(anchorY);
+          console.log(anchorY[index]);
+
           scrollViewRef.current?.getNode().scrollTo({
             y: anchorY[index],
             animated: true,
           });
         }}
         onMeasurement={(index, length) => {
-          anchorX[index] = length;
+          const _anchorX = anchorX;
+          _anchorX[index] = length;
+          setAnchorX(_anchorX);
         }}
       />
       <Content
         scrollView={scrollViewRef}
         onMeasurement={(index, length) => {
-          anchorY[index] = length;
+          const _anchorY = anchorY;
+          _anchorY[index] = length;
+          setAnchorY(_anchorY);
         }}
         onScroll={onScroll}
       />
