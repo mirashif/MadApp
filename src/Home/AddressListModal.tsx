7102d8948a4cc217b5520db870d453cd48995c83
mirashif <34467318+mirashif@users.noreply.github.com>
2022-02-02 18:28:07 +0600

    🛠

--- src/Home/AddressListModal.tsx
+++ src/Home/AddressListModal.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { KeyboardAvoidingView, Modal, Platform, Pressable } from "react-native";
+import { Modal, Pressable } from "react-native";
 import Animated, {
   Easing,
   useAnimatedStyle,
@@ -13,129 +13,134 @@ import { Box, Icon, makeStyles, Text, useTheme } from "../components";
 
 const addresses = ["Home", "Work"];
 
-const AddressListModal = () => {
+interface Props {
+  modalVisible: boolean;
+  setModalVisible: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const AddressListModal = ({ modalVisible, setModalVisible }: Props) => {
   const styles = useStyles();
   const theme = useTheme();
   const insets = useSafeAreaInsets();
 
-  const [selected, setSelected] = React.useState<number>(0);
+  const [selected, setSelected] = React.useState(0);
+  const containerHeight = useSharedValue(0);
   const offset = useSharedValue(0);
+
   const animatedStyle = useAnimatedStyle(() => {
     return {
       transform: [
         {
-          translateY: withTiming(offset.value, {
-            duration: 500,
-            easing: Easing.out(Easing.exp),
-          }),
+          translateY: withTiming(
+            modalVisible ? offset.value : -containerHeight.value,
+            {
+              duration: 500,
+              easing: Easing.out(Easing.exp),
+            }
+          ),
         },
       ],
+      opacity: withTiming(modalVisible ? 1 : 0, {
+        duration: 500,
+        easing: Easing.out(Easing.exp),
+      }),
     };
   });
 
   return (
-    <Modal transparent visible={true}>
-      <KeyboardAvoidingView
-        style={{ flex: 1 }}
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-      >
-        <Box style={styles.backdrop}>
-          <Animated.View
-            style={[
-              styles.container,
-              animatedStyle,
-              { paddingTop: insets.top },
-            ]}
-          >
-            <Box style={styles.header}>
-              <Pressable onPress={() => (offset.value = Math.random() * 500)}>
-                <Icon name="arrow-left" size={24} />
-              </Pressable>
-              <Text ml="m" fontSize={24}>
-                Delivery address
-              </Text>
-            </Box>
-
-            {/* Saved locations */}
-            {addresses.map((address, idx) => {
-              const isSelected = selected === idx;
-              return (
-                <Box key={idx} style={styles.item}>
-                  <Pressable
-                    onPress={() => undefined}
-                    style={styles.radioContainer}
-                  >
-                    <Box
-                      style={[
-                        styles.radio,
-                        isSelected ? styles.radioSelected : undefined,
-                      ]}
-                    />
-                  </Pressable>
-
-                  <Box style={styles.address}>
-                    <Text style={styles.label}>{address}</Text>
-                    <Text
-                      style={styles.street}
-                      numberOfLines={1}
-                      ellipsizeMode="tail"
-                    >
-                      123 Main St, New York
-                    </Text>
-                  </Box>
-
-                  <Pressable onPress={undefined} style={styles.editIcon}>
-                    <Icon
-                      name="edit-2"
-                      size={13}
-                      color={theme.colors.primary}
-                    />
-                  </Pressable>
-                </Box>
-              );
-            })}
-
-            {/* Current location */}
-            <Box style={styles.item}>
-              <Pressable
-                onPress={() => undefined}
-                style={styles.radioContainer}
-              >
-                <Icon name="map-pin" size={18} color={theme.colors.primary} />
-              </Pressable>
-
-              <Box style={styles.address}>
-                <Text style={styles.label}>Use Current Location</Text>
-                <Text
-                  style={styles.street}
-                  numberOfLines={1}
-                  ellipsizeMode="tail"
-                >
-                  123 Main St, New York
-                </Text>
-              </Box>
-
-              <Pressable onPress={undefined} style={styles.editIcon}>
-                <Icon name="edit-2" size={13} color={theme.colors.primary} />
-              </Pressable>
-            </Box>
-
-            {/* Add new address */}
-            <Pressable onPress={() => undefined} style={styles.addAddress}>
-              <Icon name="plus" size={23} color={theme.colors.primary} />
-              <Text
-                style={{
-                  fontSize: 14,
-                  color: theme.colors.primary,
-                  marginLeft: 12,
-                }}
-              >
-                Add a New Address
-              </Text>
+    <Modal
+      transparent
+      visible={modalVisible}
+      onRequestClose={() => setModalVisible(false)}
+    >
+      <Box style={styles.backdrop}>
+        <Animated.View
+          onLayout={(e) => {
+            containerHeight.value = e.nativeEvent.layout.height;
+          }}
+          style={[styles.container, animatedStyle, { paddingTop: insets.top }]}
+        >
+          <Box style={styles.header}>
+            <Pressable onPress={() => setModalVisible(false)}>
+              <Icon name="arrow-left" size={24} />
             </Pressable>
-          </Animated.View>
-        </Box>
-      </KeyboardAvoidingView>
+            <Text ml="m" fontSize={24}>
+              Delivery address
+            </Text>
+          </Box>
+
+          {/* Saved locations */}
+          {addresses.map((address, idx) => {
+            const isSelected = selected === idx;
+            return (
+              <Box key={idx} style={styles.item}>
+                <Pressable
+                  onPress={() => undefined}
+                  style={styles.radioContainer}
+                >
+                  <Box
+                    style={[
+                      styles.radio,
+                      isSelected ? styles.radioSelected : undefined,
+                    ]}
+                  />
+                </Pressable>
+
+                <Box style={styles.address}>
+                  <Text style={styles.label}>{address}</Text>
+                  <Text
+                    style={styles.street}
+                    numberOfLines={1}
+                    ellipsizeMode="tail"
+                  >
+                    123 Main St, New York
+                  </Text>
+                </Box>
+
+                <Pressable onPress={undefined} style={styles.editIcon}>
+                  <Icon name="edit-2" size={13} color={theme.colors.primary} />
+                </Pressable>
+              </Box>
+            );
+          })}
+
+          {/* Current location */}
+          <Box style={styles.item}>
+            <Pressable onPress={() => undefined} style={styles.radioContainer}>
+              <Icon name="map-pin" size={18} color={theme.colors.primary} />
+            </Pressable>
+
+            <Box style={styles.address}>
+              <Text style={styles.label}>Use Current Location</Text>
+              <Text
+                style={styles.street}
+                numberOfLines={1}
+                ellipsizeMode="tail"
+              >
+                123 Main St, New York
+              </Text>
+            </Box>
+
+            <Pressable onPress={undefined} style={styles.editIcon}>
+              <Icon name="edit-2" size={13} color={theme.colors.primary} />
+            </Pressable>
+          </Box>
+
+          {/* Add new address */}
+          <Pressable onPress={() => undefined} style={styles.addAddress}>
+            <Icon name="plus" size={23} color={theme.colors.primary} />
+            <Text
+              style={{
+                fontSize: 14,
+                color: theme.colors.primary,
+                marginLeft: 12,
+              }}
+            >
+              Add a New Address
+            </Text>
+          </Pressable>
+        </Animated.View>
+      </Box>
     </Modal>
   );
 };
