7df8f3a8355e185ba704d9570a9d092f472f56df
mirashif <34467318+mirashif@users.noreply.github.com>
2022-02-03 16:24:21 +0600

    fixed scrolling

--- src/Menu/MyProfile/MyProfile.tsx
+++ src/Menu/MyProfile/MyProfile.tsx
@@ -53,83 +53,91 @@ const MyProfile = observer(() => {
     <SafeArea>
       <DissmissKeyboard>
         <ScrollView showsVerticalScrollIndicator={false}>
-          <HeaderBar title="My Profile" />
-
-          {/* TODO: ADD PICKER */}
-          <Box
-            alignItems="center"
-            style={{ marginVertical: 36 }}
-            position="relative"
+          <Box // Fix: stops the touch event propagation
+            onStartShouldSetResponder={() => true}
           >
-            <Image
-              source={{
-                uri: "https://source.unsplash.com/hh3ViD0r0Rc/134x134",
-              }}
-              style={{ width: 134, height: 134, borderRadius: 134 }}
-            />
-
-            <Box
-              position="absolute"
-              alignItems="center"
-              justifyContent="center"
-              style={{
-                backgroundColor: "rgba(0, 0, 0, 0.5)",
-                width: 134,
-                height: 134,
-                borderRadius: 134,
-              }}
-            >
-              <Icon name="camera" size={46} color="#fff" />
-            </Box>
-          </Box>
-
-          <Box px="screen">
-            <Input
-              style={{ marginBottom: 20 }}
-              label="First Name"
-              value={firstName}
-              onChangeText={(value) => setFirstName(value)}
-            />
-
-            <Input
-              style={{ marginBottom: 20 }}
-              label="Last Name"
-              value={lastName}
-              onChangeText={(value) => setLastName(value)}
-            />
-
-            <Input
-              style={{ marginBottom: 20 }}
-              label="Phone"
-              value={phoneNumber}
-              onChangeText={(value) => setPhoneNumber(value)}
-            />
+            <HeaderBar title="My Profile" />
 
             {/* TODO: ADD PICKER */}
-            <Box flexDirection="row" justifyContent="space-between" mb="xl">
-              <Input
-                style={{ width: INPUT_GROUP_WIDTH }}
-                label="Gender"
-                onChangeText={() => null}
+            <Box
+              alignItems="center"
+              style={{ marginVertical: 36 }}
+              position="relative"
+            >
+              <Image
+                source={{
+                  uri: "https://source.unsplash.com/hh3ViD0r0Rc/134x134",
+                }}
+                style={{ width: 134, height: 134, borderRadius: 134 }}
               />
 
-              <Input
-                style={{ width: INPUT_GROUP_WIDTH }}
-                label="Birthday"
-                onChangeText={() => null}
-              />
+              <Box
+                position="absolute"
+                alignItems="center"
+                justifyContent="center"
+                style={{
+                  backgroundColor: "rgba(0, 0, 0, 0.5)",
+                  width: 134,
+                  height: 134,
+                  borderRadius: 134,
+                }}
+              >
+                <Icon name="camera" size={46} color="#fff" />
+              </Box>
             </Box>
 
-            <Input
-              style={{ marginBottom: 20 }}
-              label="Email"
-              value={email}
-              onChangeText={(value) => setEmail(value)}
-            />
+            <Box px="screen">
+              <Input
+                style={{ marginBottom: 20 }}
+                label="First Name"
+                value={firstName}
+                onChangeText={(value) => setFirstName(value)}
+              />
 
-            <Button style={{ marginBottom: 50 }} onPress={handleSave} size="lg">
-              Save Changes
-            </Button>
+              <Input
+                style={{ marginBottom: 20 }}
+                label="Last Name"
+                value={lastName}
+                onChangeText={(value) => setLastName(value)}
+              />
+
+              <Input
+                style={{ marginBottom: 20 }}
+                label="Phone"
+                value={phoneNumber}
+                onChangeText={(value) => setPhoneNumber(value)}
+              />
+
+              {/* TODO: ADD PICKER */}
+              <Box flexDirection="row" justifyContent="space-between" mb="xl">
+                <Input
+                  style={{ width: INPUT_GROUP_WIDTH }}
+                  label="Gender"
+                  onChangeText={() => null}
+                />
+
+                <Input
+                  style={{ width: INPUT_GROUP_WIDTH }}
+                  label="Birthday"
+                  onChangeText={() => null}
+                />
+              </Box>
+
+              <Input
+                style={{ marginBottom: 20 }}
+                label="Email"
+                value={email}
+                onChangeText={(value) => setEmail(value)}
+              />
+
+              <Button
+                style={{ marginBottom: 50 }}
+                onPress={handleSave}
+                size="lg"
+              >
+                Save Changes
+              </Button>
+            </Box>
           </Box>
         </ScrollView>
       </DissmissKeyboard>
