5e91170542ad8cb6226e1c3b3be693cecec59ad4
Samiul Haque Sohan <samiulhsohan@gmail.com>
2022-02-05 18:10:13 +0300

    add item tags

--- src/Home/HomeRestaurant.tsx
+++ src/Home/HomeRestaurant.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { ScrollView, Image, TouchableWithoutFeedback } from "react-native";
 
 import type { Theme } from "../components";
-import { Box, Icon, makeStyles, useTheme, Text } from "../components";
+import { Box, Icon, makeStyles, useTheme } from "../components";
 import type { Item as IItem } from "../state/store/ItemStore";
 import type { RestaurantType } from "../state/store/RestaurantStore";
 
@@ -50,7 +50,6 @@ const HomeRestaurant = ({
         </Box>
       </TouchableWithoutFeedback>
 
-      <Text>{items.length}</Text>
       {items.map((item) => (
         <Box key={item.id} style={styles.restaurantItem}>
           <Item item={item} onItemPress={onItemPress} />
