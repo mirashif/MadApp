8e967de31b5caf1726162297f0fb1e7f280fc969
mirashif <34467318+mirashif@users.noreply.github.com>
2022-02-09 21:00:48 +0600

    added observer and gray bg on item

--- src/Home/RestaurantMenu/RestaurantMenu.tsx
+++ src/Home/RestaurantMenu/RestaurantMenu.tsx
@@ -1,3 +1,4 @@
+import { observer } from "mobx-react";
 import React, { useEffect, useRef, useState } from "react";
 import { View } from "react-native";
 import type Animated from "react-native-reanimated";
@@ -18,90 +19,92 @@ import HeaderImage from "./HeaderImage";
 import Offer from "./Offer";
 import TabHeader from "./TabHeader";
 
-const RestaurantMenu = ({ route }: HomeStackProps<"RestaurantMenu">) => {
-  const y = useSharedValue(0);
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [anchorX, setAnchorX] = useState<number[]>([]);
-  const [anchorY, setAnchorY] = useState<number[]>([]);
-  const scrollViewRefX = useRef<Animated.ScrollView>(null);
-  const scrollViewRef = useRef<Animated.ScrollView>(null);
+const RestaurantMenu = observer(
+  ({ route }: HomeStackProps<"RestaurantMenu">) => {
+    const y = useSharedValue(0);
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [anchorX, setAnchorX] = useState<number[]>([]);
+    const [anchorY, setAnchorY] = useState<number[]>([]);
+    const scrollViewRefX = useRef<Animated.ScrollView>(null);
+    const scrollViewRef = useRef<Animated.ScrollView>(null);
 
-  const scrollHandler = useAnimatedScrollHandler((event) => {
-    y.value = event.contentOffset.y;
-  });
-
-  const handleActiveIndex = (v: number) => {
-    anchorY.forEach((_, i) => {
-      if (v < Math.floor(anchorY[1])) setActiveIndex(0);
-      else if (v > Math.floor(anchorY[i]) && v < Math.floor(anchorY[i + 1]))
-        setActiveIndex(i);
-      else if (v >= Math.floor(anchorY[anchorY.length - 1]))
-        setActiveIndex(anchorY.length - 1);
+    const scrollHandler = useAnimatedScrollHandler((event) => {
+      y.value = event.contentOffset.y;
     });
-  };
 
-  useDerivedValue(() => {
-    runOnJS(handleActiveIndex)(y.value);
-  });
+    const handleActiveIndex = (v: number) => {
+      anchorY.forEach((_, i) => {
+        if (v < Math.floor(anchorY[1])) setActiveIndex(0);
+        else if (v > Math.floor(anchorY[i]) && v < Math.floor(anchorY[i + 1]))
+          setActiveIndex(i);
+        else if (v >= Math.floor(anchorY[anchorY.length - 1]))
+          setActiveIndex(anchorY.length - 1);
+      });
+    };
 
-  // useEffect(() => {
-  //   scrollViewRefX.current?.getNode().scrollTo({
-  //     x: anchorX[activeIndex],
-  //     animated: true,
-  //   });
-  // }, [activeIndex, anchorX]);
+    useDerivedValue(() => {
+      runOnJS(handleActiveIndex)(y.value);
+    });
 
-  const { restaurantId } = route.params;
-  const restaurants: RestaurantStore = useAppState("restaurants");
-  const restaurant = restaurants.get(restaurantId);
+    // useEffect(() => {
+    //   scrollViewRefX.current?.getNode().scrollTo({
+    //     x: anchorX[activeIndex],
+    //     animated: true,
+    //   });
+    // }, [activeIndex, anchorX]);
 
-  if (!restaurant) return null;
-  return (
-    <SafeArea>
-      <View>
-        <HeaderImage
-          y={y}
-          restaurantName={restaurant.data.name}
-          imageURI={restaurant.data.bannerImageURI}
+    const { restaurantId } = route.params;
+    const restaurants: RestaurantStore = useAppState("restaurants");
+    const restaurant = restaurants.get(restaurantId);
+
+    if (!restaurant) return null;
+    return (
+      <SafeArea>
+        <View>
+          <HeaderImage
+            y={y}
+            restaurantName={restaurant.data.name}
+            imageURI={restaurant.data.bannerImageURI}
+          />
+          <Offer
+            y={y}
+            title={restaurant.bannerTitle}
+            description={restaurant.bannerDescription}
+            phone={restaurant.data.phone}
+          />
+        </View>
+        <TabHeader
+          scrollViewRefX={scrollViewRefX}
+          activeIndex={activeIndex}
+          onTabPress={
+            (_) => undefined
+            // (index: number) => {
+            //   scrollViewRef.current?.getNode().scrollTo({
+            //     y: anchorY[index],
+            //     animated: true,
+            //   });
+            // }
+          }
+          onMeasurement={(index, length) => {
+            const _anchorX = anchorX;
+            _anchorX[index] = length;
+            setAnchorX(_anchorX);
+          }}
+          categories={restaurant?.categories || []}
         />
-        <Offer
-          y={y}
-          title={restaurant.bannerTitle}
-          description={restaurant.bannerDescription}
-          phone={restaurant.data.phone}
+        <Content
+          scrollViewRef={scrollViewRef}
+          onMeasurement={(index, length) => {
+            const _anchorY = anchorY;
+            _anchorY[index] = length;
+            setAnchorY(_anchorY);
+          }}
+          onScroll={scrollHandler}
+          categories={restaurant?.categories || []}
         />
-      </View>
-      <TabHeader
-        scrollViewRefX={scrollViewRefX}
-        activeIndex={activeIndex}
-        onTabPress={
-          (_) => undefined
-          // (index: number) => {
-          //   scrollViewRef.current?.getNode().scrollTo({
-          //     y: anchorY[index],
-          //     animated: true,
-          //   });
-          // }
-        }
-        onMeasurement={(index, length) => {
-          const _anchorX = anchorX;
-          _anchorX[index] = length;
-          setAnchorX(_anchorX);
-        }}
-        categories={restaurant?.categories || []}
-      />
-      <Content
-        scrollViewRef={scrollViewRef}
-        onMeasurement={(index, length) => {
-          const _anchorY = anchorY;
-          _anchorY[index] = length;
-          setAnchorY(_anchorY);
-        }}
-        onScroll={scrollHandler}
-        categories={restaurant?.categories || []}
-      />
-    </SafeArea>
-  );
-};
+      </SafeArea>
+    );
+  }
+);
 
 export default RestaurantMenu;
