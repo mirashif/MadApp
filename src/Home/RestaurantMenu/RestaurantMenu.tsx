2e42341fb0268682ff960fb3b0cd10f88d400072
mirashif <34467318+mirashif@users.noreply.github.com>
2022-02-13 22:00:33 +0600

    🛠todo: auto active id

--- src/Home/RestaurantMenu/RestaurantMenu.tsx
+++ src/Home/RestaurantMenu/RestaurantMenu.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
-import type { ScrollView } from "react-native";
+import type {
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+  ScrollView,
+} from "react-native";
 import { View } from "react-native";
 import { useAnimatedRef, useSharedValue } from "react-native-reanimated";
 
@@ -7,7 +11,9 @@ import type { HomeStackProps } from "..";
 import { SafeArea, Text } from "../../components";
 import { useAppState } from "../../state/StateContext";
 import type { RestaurantStore } from "../../state/store/RestaurantStore";
+import ItemBottomSheet from "../ItemBottomSheet/ItemBottomSheet";
 
+import Content from "./Content";
 import HeaderImage from "./HeaderImage";
 import Offer from "./Offer";
 import TabHeader from "./TabHeader";
@@ -22,40 +28,69 @@ const RestaurantMenu = ({ route }: HomeStackProps<"RestaurantMenu">) => {
   const description = restaurant?.bannerDescription;
   const phone = restaurant?.data.phone;
   const categories = restaurant?.categories;
+
   // animated values
-  const tabRef = useAnimatedRef<ScrollView>();
   const contentScroll = useSharedValue(0);
+  const tabRef = useAnimatedRef<ScrollView>();
+  const contentRef = useAnimatedRef<ScrollView>();
+
   // react state values
+  const [bottomSheetItemId, setBottomSheetItemId] = useState<string | null>(
+    null
+  );
   const [activeId, setActiveId] = useState<string | undefined>(
     categories?.[0]?.id
   );
-  const [tabs, setTabs] = useState<
+  const [tabMeasurements, setTabMeasurements] = useState<
     {
       categoryId: string;
       x: number;
     }[]
   >([]);
+  const [contentMeasurements, setContentMeasurements] = useState<
+    {
+      categoryId: string;
+      y: number;
+    }[]
+  >([]);
 
-  const handleMeasurement = (categoryId: string, x: number) => {
-    const newTab = { categoryId, x };
-    setTabs((oldTabs) =>
-      oldTabs.indexOf(newTab) === -1 ? [...oldTabs, newTab] : oldTabs
-    );
+  const handleScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    const { contentOffset } = event.nativeEvent;
+    contentScroll.value = contentOffset.x;
+  };
+
+  const handleMeasurement = (categoryId: string, x?: number, y?: number) => {
+    if (x)
+      setTabMeasurements((prev) =>
+        prev.indexOf({ categoryId, x }) === -1
+          ? [...prev, { categoryId, x }]
+          : prev
+      );
+    if (y)
+      setContentMeasurements((prev) =>
+        prev.indexOf({ categoryId, y }) === -1
+          ? [...prev, { categoryId, y }]
+          : prev
+      );
   };
 
   const handleTabPress = (categoryId: string) => {
     setActiveId(categoryId);
   };
 
+  const handleItemPress = (itemId: string) => {
+    setBottomSheetItemId(itemId);
+  };
+
   useEffect(() => {
-    const found = tabs.find((tab) => tab.categoryId === activeId);
+    const found = tabMeasurements.find((tab) => tab.categoryId === activeId);
     if (found)
       tabRef.current?.scrollTo({
         x: found.x,
         y: 0,
         animated: true,
       });
-  }, [activeId, tabRef, tabs]);
+  }, [activeId, tabRef, tabMeasurements]);
 
   if (!restaurant) return null;
   return (
@@ -69,7 +104,21 @@ const RestaurantMenu = ({ route }: HomeStackProps<"RestaurantMenu">) => {
         onTabPress={handleTabPress}
         {...{ activeId, categories, tabRef }}
       />
-      <Text>{JSON.stringify(tabs, null, 2)}</Text>
+      <Content
+        onMeasurement={handleMeasurement}
+        onItemPress={handleItemPress}
+        onScroll={handleScroll}
+        {...{
+          categories,
+          contentRef,
+        }}
+      />
+      <ItemBottomSheet
+        {...{
+          bottomSheetItemId,
+          setBottomSheetItemId,
+        }}
+      />
     </SafeArea>
   );
 };
