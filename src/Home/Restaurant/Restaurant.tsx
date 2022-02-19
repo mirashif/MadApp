c54d73806e1bd3021e046da1afbc662ee35542f8
Samiul Haque Sohan <samiulhsohan@gmail.com>
2022-02-19 20:27:51 +0300

    add floating cart on restaurant page

--- src/Home/Restaurant/Restaurant.tsx
+++ src/Home/Restaurant/Restaurant.tsx
@@ -17,6 +17,7 @@ import type { HomeStackProps } from "..";
 import { SafeArea } from "../../components";
 import { useAppState } from "../../state/StateContext";
 import type { RestaurantStore } from "../../state/store/RestaurantStore";
+import FloatingCart from "../FloatingCart";
 import { ItemBuilder } from "../ItemBuilder";
 
 import type { IMeasurement } from "./constants";
@@ -174,6 +175,7 @@ const Restaurant = observer(({ route }: HomeStackProps<"Restaurant">) => {
   if (!restaurant) return null;
   return (
     <SafeArea>
+      <FloatingCart />
       <View>
         <HeaderImage {...{ contentScroll, restaurantName, imageURI }} />
         <Offer {...{ contentScroll, title, description, phone }} />
