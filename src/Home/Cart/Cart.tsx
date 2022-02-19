976d4d8cc7374b3d096dbba45d2e380de7888054
Samiul Haque Sohan <samiulhsohan@gmail.com>
2022-02-19 16:21:07 +0300

    refactor total

--- src/Home/Cart/Cart.tsx
+++ src/Home/Cart/Cart.tsx
@@ -1,25 +1,23 @@
 import { observer } from "mobx-react";
 import React, { useState } from "react";
-import { Dimensions, ScrollView } from "react-native";
+import { ScrollView } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-import type { HomeStackProps } from "..";
-import { Box, HeaderBar, SafeArea, Text } from "../../components";
+import { Box, HeaderBar, SafeArea } from "../../components";
 import { useAppState } from "../../state/StateContext";
 import type { CartStore, CouponEditor } from "../../state/store/CartStore";
 import LocationBar from "../LocationBar";
 
 import ApplyCouponModal from "./ApplyCouponModal";
 import Breakdown from "./Breakdown";
-import { CheckoutButton } from "./Button";
 import CartItems from "./CartItems";
 import InvalidCouponModal from "./InvalidCouponModal";
+import Total from "./Total";
 import UpsellItems from "./UpsellItems";
 import Voucher from "./Voucher";
 
-const Cart = observer(({ navigation }: HomeStackProps<"Cart">) => {
+const Cart = observer(() => {
   const insets = useSafeAreaInsets();
-  const windowWidth = Dimensions.get("window").width;
 
   const cart: CartStore = useAppState("cart");
   const editor: CouponEditor = cart.couponEditor;
@@ -37,43 +35,18 @@ const Cart = observer(({ navigation }: HomeStackProps<"Cart">) => {
       >
         <HeaderBar title="Cart" />
         <LocationBar editMode onEditPress={() => null} />
-
         <CartItems />
         <UpsellItems />
 
-        {/*Order Summary & Voucher*/}
+        {/*Order breakdown & Voucher*/}
         <Box style={{ marginTop: 83, paddingHorizontal: 40 }}>
           <Breakdown />
           <Voucher {...{ setApplyCouponModalVisible }} />
         </Box>
       </ScrollView>
 
-      {/*Total & Checkout*/}
-      <Box
-        position="absolute"
-        bottom={0}
-        width={windowWidth}
-        backgroundColor="background"
-        style={{ paddingTop: 22, paddingBottom: 16 + insets.bottom }}
-      >
-        <Box
-          flexDirection="row"
-          justifyContent="space-between"
-          alignItems="center"
-          style={{ paddingHorizontal: 40 }}
-        >
-          <Box>
-            <Text style={{ fontSize: 18 }}>Total</Text>
-            <Text style={{ color: "#BBBBBB", fontSize: 11 }}>VAT included</Text>
-          </Box>
-
-          <Text style={{ fontSize: 18 }}>৳{cart.grandTotalAmount}</Text>
-        </Box>
-
-        <Box style={{ alignItems: "center", marginTop: 27 }}>
-          <CheckoutButton onPress={() => navigation.navigate("Checkout")} />
-        </Box>
-      </Box>
+      {/* Grand total & checkout */}
+      <Total />
 
       <ApplyCouponModal
         visible={applyCouponModalVisible}
