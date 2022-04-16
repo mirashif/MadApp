a98a788bd39b37cec16793dbb0401073faa17205
Mir Ashif <34467318+mirashif@users.noreply.github.com>
2022-04-16 17:09:07 +0600

    added user settings (#25)

--- src/Menu/Settings/Settings.tsx
+++ src/Menu/Settings/Settings.tsx
@@ -1,29 +1,41 @@
-import React, { useState } from "react";
+import React from "react";
 import { ScrollView } from "react-native";
 
 import { HeaderBar, SafeArea } from "../../components";
+import { useAppState } from "../../state/StateContext";
+import type { SettingsType, UserStore } from "../../state/store/UserStore";
 
 import SwitchItem from "./SwitchItem";
 
 const Settings = () => {
-  const [isNotificationEnabled, setIsNotificationEnabled] = useState(false);
-  const [isEmailEnabled, setIsEmailEnabled] = useState(false);
+  const user: UserStore = useAppState("user");
+  const settings: SettingsType = user.settings;
+  const receivePush = settings.receivePush;
+  const receiveEmail = settings.receiveEmail;
 
   return (
     <SafeArea>
       <ScrollView>
         <HeaderBar title="Settings" />
-
         <SwitchItem
           title="Receive Push Notifications"
-          value={isNotificationEnabled}
-          onValueChange={() => setIsNotificationEnabled(!isNotificationEnabled)}
+          value={receivePush ?? false}
+          onValueChange={async () =>
+            await user.updateSettings({
+              ...settings,
+              receivePush: !receivePush,
+            })
+          }
         />
-
         <SwitchItem
           title="Receive Offers by Email"
-          value={isEmailEnabled}
-          onValueChange={() => setIsEmailEnabled(!isEmailEnabled)}
+          value={receiveEmail ?? false}
+          onValueChange={async () =>
+            await user.updateSettings({
+              ...settings,
+              receiveEmail: !receiveEmail,
+            })
+          }
         />
       </ScrollView>
     </SafeArea>
